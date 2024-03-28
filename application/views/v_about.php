3e56897c1317b5014acf753577f98946f2feadc5
Yusuf Malik S <yusufmaliksujatmiko2@gmail.com>
2024-03-28 14:02:14 +0700

    wp2 pertemuan3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Yusuf</li>
+        <li>Nama Belakang : Malik</li>
+    </ul>
+ <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Metland Cileungsi blok CK1 no.17</li>
+    </ul>
+ 
+    <h4>Tempat, Tanggal Lahir</h4>
+    <ul type="none">
+        <li>Jakarta, 30 Juni 2002</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>Cycling</li>
+    </ul>
+</section>
